26e15746df28772ee499ef2490f6f285210ca3ab
Aman <amanmalik3105@gmail.com>
2023-02-09 22:37:12 +0530

    This is the basic setup , all the files are made and the code will be further updated in the upcming commits

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  
+}
+
+export default App;
